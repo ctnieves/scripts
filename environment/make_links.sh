c4253d9c56b44fc1fbbe181736882cce3cc176e0
Christian Nieves <christian@nieves.io>
2017-11-24 20:15:05 -0700

    Add make_links.sh

---
+++ environment/make_links.sh
@@ -0,0 +1,14 @@
+ln -s ~/Dropbox/dev ~/ # symlink development directory to home folder
+ln -s ~/Dropbox/dev/go ~/go # symlink development directory to home folder
+ln -s ~/Dropbox/dotfiles/vim ~/.vim                 # symlink vim directory
+ln -s ~/Dropbox/dotfiles/.vimrc ~/.vimrc            # symlink vim configuration
+ln -s ~/Dropbox/dotfiles/.tmux ~/.tmux              # symlink tmux directory
+ln -s ~/Dropbox/dotfiles/.tmux.conf ~/.tmux.conf    # symlink tmux configuration
+
+ln -s ~/Dropbox/dotfiles/.grc ~/.grc    # symlink grc configuration
+
+ln -s ~/Dropbox/dotfiles/.bash_profile ~/.bash_profile    # symlink bash profile/aliases
+ln -s ~/Dropbox/dotfiles/.bash-powerline.sh ~/.bash-powerline.sh    # symlink bash profile/aliases
+
+ln -s ~/Dropbox/dotfiles/.gitconfig ~/.gitconfig                  # symlink git configuration/aliases
+ln -s ~/Dropbox/dotfiles/.gitignore_global ~/.gitignore_global    # symlink global gitignore
