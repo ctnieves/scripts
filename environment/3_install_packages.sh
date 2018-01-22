ab8642166a0d420101e3f4a0eaa391fe3000f6f9
Christian Nieves <christian@nieves.io>
2018-01-22 11:46:39 -0700

    Add colordiff to packages

--- environment/3_install_packages.sh
+++ environment/3_install_packages.sh
@@ -13,11 +13,13 @@ brew doctor
 # necessary bottles
 #brew install macvim --with-override-system-vim  # install correct version of Vim
 brew install vim --override-system-vi
+brew install gnu-sed --with-default-names
 
 brew install reattach-to-user-namespace # required for macOS clipboard integration with Vim
 
 #generic colorizer
 brew install grc
+brew install colordiff
 
 # setup ruby environment
 brew install rbenv
